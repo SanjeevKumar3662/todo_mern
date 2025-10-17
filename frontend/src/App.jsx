22e8b5f239d16dc1e786ebb0df9876d5ba24699f
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2025-10-17 19:00:05 +0530

    added form validation and some css styles

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,21 +10,41 @@ function App() {
         <form action="" method="get">
           <div className="input-field">
             <label htmlFor="name">Enter your Name : </label>
-            <input type="text" id="name" name="name" />
+            <input type="text" id="name" name="name" placeholder="Enter Name" />
           </div>
 
           <div className="input-field">
             <label htmlFor="age">Enter Age : </label>
-            <input type="number" name="age" id="age" />
+            <input
+              type="number"
+              name="age"
+              id="age"
+              min="0"
+              max="200"
+              defaultValue={0}
+              placeholder="Enter Age"
+            />
           </div>
 
           <div className="input-field">
             <label htmlFor="location">Enter Location : </label>
-            <input type="text" name="location" id="location" />
+            <input
+              type="text"
+              name="location"
+              id="location"
+              defaultValue={"New Delhi"}
+              placeholder="Enter Location"
+            />
           </div>
           <div className="input-field">
             <label htmlFor="email">Enter Email : </label>
-            <input type="email" name="email" id="email" />
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Enter  Email"
+              defaultValue={"test@example.com"}
+            />
           </div>
           <button type="submit">Submit</button>
         </form>
