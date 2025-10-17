c4cdb29bcf095d42ed7c7dee658afd5433e4fd46
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2025-10-18 00:36:47 +0530

    now /todo post endpoint works , and submits to db

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,38 @@
 import express from "express";
 import mongoose from "mongoose";
 import { configDotenv } from "dotenv";
+import cors from "cors";
 
 const app = express();
+app.use(cors());
 
 const PORT = 3000;
 const DB_URI = configDotenv().parsed.DB_URI;
 
+//importing todo model
+import Todo from "./models/todo.model.js";
+
+express.urlencoded({ extended: true });
+app.use(express.json());
+
+//test endpoint
 app.get("/", (req, res) => {
   res.status(200).send("<h1>this is the / homepage</h1>");
 });
 
+//create a todo
+app.post("/todos", async (req, res) => {
+  const { title, description } = req.body;
+  try {
+    await Todo.create({ title, description });
+    res.status(200).json({ message: "Created Successfully" });
+    // console.log(title, "created");
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+//db connection
 const connectDb = async () => {
   try {
     await mongoose.connect(DB_URI);
