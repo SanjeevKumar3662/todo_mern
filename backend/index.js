8d103bc5d0c91332c5f3e9bdfee7c5926f96562c
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2025-10-17 19:31:36 +0530

    basic sever with node.js express mongoose nodemon setup

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.status(200).send("<h1>server's homepage</h1>");
+});
+
+app.listen(PORT, () => {
+  console.log("server Started");
+});
