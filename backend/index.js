7740590c7deec072507cdf6c65763643eb269c99
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2025-10-17 22:43:38 +0530

    now server connects to db

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,27 @@
 import express from "express";
+import mongoose from "mongoose";
+import { configDotenv } from "dotenv";
 
 const app = express();
 
 const PORT = 3000;
+const DB_URI = configDotenv().parsed.DB_URI;
 
 app.get("/", (req, res) => {
   res.status(200).send("<h1>this is the / homepage</h1>");
 });
 
-app.listen(PORT, () => {
-  console.log("Sever Started !");
-});
+const connectDb = async () => {
+  try {
+    await mongoose.connect(DB_URI);
+    console.log("connected to DB!");
+    app.listen(PORT, () => {
+      console.log("Sever Started !");
+      // console.log(DB_URI);
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+connectDb();
