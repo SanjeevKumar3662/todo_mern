d57e1f31efb6a1d6e8b33301f7d2443dc825cdb1
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2025-10-18 00:35:15 +0530

    extracted the form into a component , now todo submission works

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,4 @@
+import Form from "./components/form";
 import "./App.css";
 
 function App() {
@@ -7,31 +8,7 @@ function App() {
     <>
       <div className="container">
         <h1>This Is A MERN Todo List</h1>
-
-        <form action="" method="get">
-          <div className="input-field">
-            <label htmlFor="name">Title : </label>
-            <input
-              type="text"
-              id="name"
-              name="name"
-              placeholder="Enter Subject Title"
-            />
-          </div>
-
-          <div className="input-field">
-            <label htmlFor="discription">Enter Discription : </label>
-            <textarea
-              name="discription"
-              id=""
-              cols="30"
-              rows="2"
-              placeholder="You can write anything here"
-              maxLength={2000}
-            ></textarea>
-          </div>
-          <button type="submit">Submit</button>
-        </form>
+        <Form />
       </div>
     </>
   );
