b3fd4bfba208d49fe535866fce701cc13ddd6446
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2025-10-18 21:35:22 +0530

    moved onClickDelete evntlistner to Todos component from Todo component

--- frontend/src/components/Todos.jsx
+++ frontend/src/components/Todos.jsx
@@ -15,6 +15,28 @@ const Todos = () => {
     getTodos();
   }, []);
 
+  const onClickDelete = async (id) => {
+    try {
+      const response = await fetch(`http://localhost:3000/todos/${id}`, {
+        method: "DELETE",
+        headers: { "Content-Type": "application/json" },
+      });
+
+      // console.log(id);
+      if (response.ok) {
+        //   setShowPopup(true); // show popup
+        console.log("item deleted");
+
+        //this will filter and remove the deleted todo for client side
+        setTodos(() => todos.filter((todo) => todo._id != id));
+        //   // hide popup after 2 seconds
+        //   setTimeout(() => setShowPopup(false), 2000);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   todos && console.log(todos);
   return (
     <>
@@ -30,6 +52,7 @@ const Todos = () => {
               id={obj._id}
               title={obj.title}
               description={obj.description}
+              onClickDelete={onClickDelete}
             />
           );
         })}
--- frontend/src/components/todo/Todo.jsx
+++ frontend/src/components/todo/Todo.jsx
@@ -1,33 +1,14 @@
 // import { useState } from "react";
 import "./todo.css";
-const Todo = ({ id, title }) => {
+const Todo = ({ id, title, onClickDelete }) => {
   // const [showPopup, setShowPopup] = useState(false);
-
-  const onClickDelete = async () => {
-    try {
-      const response = await fetch(`http://localhost:3000/todos/${id}`, {
-        method: "DELETE",
-        headers: { "Content-Type": "application/json" },
-      });
-
-      console.log(id);
-      if (response.ok) {
-        //   setShowPopup(true); // show popup
-        console.log("item deleted");
-        //   // hide popup after 2 seconds
-        //   setTimeout(() => setShowPopup(false), 2000);
-      }
-    } catch (err) {
-      console.log(err);
-    }
-  };
 
   return (
     <div className="todo">
       <span>{title}</span>
       <div>
         <button className="">More</button>
-        <button onClick={onClickDelete} className="delete">
+        <button onClick={() => onClickDelete(id)} className="delete">
           Delete
         </button>
       </div>
