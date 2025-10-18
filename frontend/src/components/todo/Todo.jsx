59b9436485ce94f9a9b70c42b901d29ce5801535
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2025-10-18 22:08:55 +0530

    now more button show toggles description

--- frontend/src/components/todo/Todo.jsx
+++ frontend/src/components/todo/Todo.jsx
@@ -1,17 +1,27 @@
-// import { useState } from "react";
 import "./todo.css";
-const Todo = ({ id, title, onClickDelete }) => {
-  // const [showPopup, setShowPopup] = useState(false);
+import { useState } from "react";
 
+const Todo = ({ id, title, onClickDelete, description }) => {
+  const [showMore, setShowMore] = useState(false);
+
+  const onClickShowMore = () => {
+    setShowMore(() => !showMore);
+  };
   return (
     <div className="todo">
-      <span>{title}</span>
-      <div>
-        <button className="">More</button>
-        <button onClick={() => onClickDelete(id)} className="delete">
-          Delete
-        </button>
+      <div className="title">
+        <span>{title}</span>
+        <div>
+          <button onClick={onClickShowMore} className="">
+            More
+          </button>
+          <button onClick={() => onClickDelete(id)} className="delete">
+            Delete
+          </button>
+        </div>
       </div>
+
+      {showMore && <pre className="description">{description}</pre>}
     </div>
   );
 };
