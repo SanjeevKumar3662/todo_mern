edb5c597e0ba7f42253e74702290bf9fc953df73
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2025-10-18 20:54:00 +0530

    added eventlistner for deleting a todo

--- frontend/src/components/todo/Todo.jsx
+++ frontend/src/components/todo/Todo.jsx
@@ -1,9 +1,36 @@
+// import { useState } from "react";
 import "./todo.css";
+const Todo = ({ id, title }) => {
+  // const [showPopup, setShowPopup] = useState(false);
 
-const Todo = ({ title }) => {
+  const onClickDelete = async () => {
+    try {
+      const response = await fetch(`http://localhost:3000/todos/${id}`, {
+        method: "DELETE",
+        headers: { "Content-Type": "application/json" },
+      });
+
+      console.log(id);
+      if (response.ok) {
+        //   setShowPopup(true); // show popup
+        console.log("item deleted");
+        //   // hide popup after 2 seconds
+        //   setTimeout(() => setShowPopup(false), 2000);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div className="todo">
       <span>{title}</span>
+      <div>
+        <button className="">More</button>
+        <button onClick={onClickDelete} className="delete">
+          Delete
+        </button>
+      </div>
     </div>
   );
 };
