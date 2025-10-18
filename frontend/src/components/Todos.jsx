6606f59b02c49735cb0e5af967513b5196ce7db4
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2025-10-18 16:48:20 +0530

    capitalized the first char

---
+++ frontend/src/components/Todos.jsx
@@ -0,0 +1,39 @@
+import { useEffect } from "react";
+import Todo from "./todo/Todo";
+import { useState } from "react";
+
+const Todos = () => {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    const getTodos = async () => {
+      const res = await fetch("http://localhost:3000/todos");
+      // http://localhost:3000/
+      const data = await res.json();
+      setTodos(data);
+    };
+    getTodos();
+  }, []);
+
+  todos && console.log(todos);
+  return (
+    <>
+      <h1>All Todos Here</h1>
+
+      {/* <Todo title={"anki"} />
+       */}
+      {todos &&
+        todos.map((obj) => {
+          return (
+            <Todo
+              key={obj._id}
+              title={obj.title}
+              description={obj.description}
+            />
+          );
+        })}
+    </>
+  );
+};
+
+export default Todos;
