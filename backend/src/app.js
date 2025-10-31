8184270d6ce4a4d49a2be6e9783d1b24b6acbc3c
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2025-10-31 16:03:40 +0530

    added userRouter to handle /api/v1/todos routes

---
+++ backend/src/app.js
@@ -0,0 +1,20 @@
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+
+import userRouter from "./routes/todo.routes.js";
+
+express.urlencoded({ extended: true });
+app.use(express.json());
+
+//test endpoint
+app.get("/", (req, res) => {
+  res.status(200).send("<h1>this is the / homepage</h1>");
+});
+
+app.use("/api/v1/todos", userRouter);
+
+export default app;
