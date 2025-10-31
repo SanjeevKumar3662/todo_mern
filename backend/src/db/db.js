05a2af03562a8dd089ceac1e404f32f0232331a7
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2025-10-31 21:42:53 +0530

    importing DB_NAME from constants.js

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -1,9 +1,10 @@
 import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
 const DB_URI = process.env.DB_URI;
 
 const connectDb = async () => {
   try {
-    const db = await mongoose.connect(`${DB_URI}`);
+    const db = await mongoose.connect(`${DB_URI}/${DB_NAME}`);
     // isConnected = db.connections[0].readyState === 1;
     console.log("MongoDB connected!");
   } catch (error) {
