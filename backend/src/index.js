d4a0928fb8c4bdd004617b4eff511177bb85098f
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2025-10-31 19:03:36 +0530

    now importing db_name from contants with db_uri for db connection

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,13 +2,14 @@ import mongoose from "mongoose";
 
 const PORT = 3000;
 const DB_URI = process.env.DB_URI;
+import { DB_NAME } from "./constants.js";
 
 import app from "./app.js";
 
 //db connection
 const connectDb = async () => {
   try {
-    await mongoose.connect(DB_URI);
+    await mongoose.connect(`${DB_URI}/${DB_NAME}`);
     console.log("connected to DB!");
     app.listen(PORT, () => {
       console.log("Sever Started !");
