466f9c5bfc4a26446a615beb718609efe5e1c28d
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2025-11-01 23:37:32 +0530

    added a user/auth-test route

--- backend/src/routes/users.routes.js
+++ backend/src/routes/users.routes.js
@@ -1,9 +1,15 @@
 import { Router } from "express";
 import { loginUser, registerUser } from "../controllers/user.controller.js";
+import { authenticateToken } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
+router.route("/auth-test").post(authenticateToken, (req, res) => {
+  return res
+    .status(200)
+    .json({ message: "user is authenticated", payload: req.user });
+});
 
 export default router;
