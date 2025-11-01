f0d93add3a6ae8ccbe50d7a513f447ff6d6c5e3e
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2025-11-01 20:25:17 +0530

    added loginUser controller

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -11,7 +11,7 @@ export const registerUser = async (req, res) => {
 
   try {
     const { username, email, fullname, password } = req.body;
-    console.log({ username, email, fullname, password });
+    // console.log({ username, email, fullname, password });
 
     //checking if all fields are provided
     if (!(username && email && fullname && password)) {
@@ -45,5 +45,35 @@ export const registerUser = async (req, res) => {
     });
   } catch (error) {
     res.status(500).json({ error: error.message });
+  }
+};
+
+export const loginUser = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    if (!(username && password)) {
+      return res
+        .status(400)
+        .json("Both username and password are required for login");
+    }
+
+    //check if user exists
+    const user = await User.findOne({ username }).select("+password");
+    if (!user) {
+      return res.status(400).json("User not found / exist");
+    }
+
+    //compare/authenticate password
+    const isPasswordVerified = await bcrypt.compare(password, user.password);
+    if (!isPasswordVerified) {
+      return res.status(401).json("Incorrect password");
+    }
+
+    // console.log(user, isPasswordVerified);
+    return res.status(200).json("loginIn successfull");
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: error.message });
   }
 };
