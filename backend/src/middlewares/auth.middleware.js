6baa8846c39f76acbd67883526b6c334aebbd6a1
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2025-11-03 00:32:47 +0530

    refactor(auth.middleware): enhance error handling and support cookie-based access token

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -2,7 +2,8 @@ import jwt from "jsonwebtoken";
 
 export const authenticateToken = (req, res, next) => {
   try {
-    const inputAccessToken = req.headers.authorization?.split(" ")[1];
+    const inputAccessToken =
+      req.cookies.accessToken || req.headers.authorization?.split(" ")[1];
 
     if (!inputAccessToken) {
       return res.status(401).json("accessToken not received");
@@ -11,15 +12,20 @@ export const authenticateToken = (req, res, next) => {
     jwt.verify(inputAccessToken, process.env.API_ACCESS_KEY, (err, user) => {
       // here user is the payload from inputAccessToken
       if (err) {
-        return res.status(401).json("accessToken expired or wrong");
+        return res
+          .status(401)
+          .json({ status: "accessToken expired or wrong", err: err.message });
       }
 
       req.user = user; // after auth, we can access this user from payload via req.user
+      // console.log("jwt.verify ->", user);
       next();
     });
 
     // console.log(inputAccessToken);
   } catch (error) {
-    res.status(401).json("accessToken expired or wrong");
+    res
+      .status(401)
+      .json({ status: "accessToken expired or wrong", message: error.message });
   }
 };
