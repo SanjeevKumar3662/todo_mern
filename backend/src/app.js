706eecd7343627e1e4eb61359f89c24825c5bac9
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2025-11-03 20:07:35 +0530

    feat(cors): configure CORS with specific origins and credentials support

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,16 @@ import userRouter from "./routes/users.routes.js";
 import cookieParser from "cookie-parser";
 
 const app = express();
-app.use(cors());
+app.use(
+  cors(
+    {
+      origin: ["https://todo-mern-app-pi.vercel.app/", "http://localhost:5173"],
+    },
+    {
+      credentials: true,
+    }
+  )
+);
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
