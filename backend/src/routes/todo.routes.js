4d322505deefb4c980dfdbf803e210537dd8a486
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2025-11-03 18:55:03 +0530

    feat(todo.routes): add authentication middleware to create and delete todo routes

--- backend/src/routes/todo.routes.js
+++ backend/src/routes/todo.routes.js
@@ -4,11 +4,12 @@ import {
   createTodo,
   deleteTodo,
 } from "../controllers/todo.controller.js";
+import { authenticateToken } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router.route("/all-todos").get(getAlltodos);
-router.route("/create").post(createTodo);
-router.route("/delete/:id").delete(deleteTodo);
+router.route("/create").post(authenticateToken, createTodo);
+router.route("/delete/:id").delete(authenticateToken, deleteTodo);
 
 export default router;
