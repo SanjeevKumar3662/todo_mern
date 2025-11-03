fd564eadb67109fb4fe982eeb64955d752181ee0
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2025-11-03 18:56:35 +0530

    fix(users.routes): reorder refresh-token route for better organization

--- backend/src/routes/users.routes.js
+++ backend/src/routes/users.routes.js
@@ -11,14 +11,15 @@ const router = Router();
 
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
+
+router.route("/refresh-token").post(refreshAccessToken);
+
+//secured routes
 router.route("/auth-test").post(authenticateToken, (req, res) => {
   return res
     .status(200)
     .json({ message: "user is authenticated", payload: req.user });
 });
-router.route("/refresh-token").post(refreshAccessToken);
-
-//secured routes
 router.route("/logout").delete(authenticateToken, logoutUser);
 
 export default router;
