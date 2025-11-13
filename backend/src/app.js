efe2ca37659acec3468d03f4585cf585e4421d4f
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2025-11-13 15:04:59 +0530

    fix: add updated cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,7 +9,7 @@ import cookieParser from "cookie-parser";
 const app = express();
 app.use(
   cors({
-    origin: ["https://todo-mern-app-pi.vercel.app", "http://localhost:5173"],
+    origin: ["https://todo.sanjeevkumar.site", "http://localhost:5173"],
     credentials: true,
   })
 );
